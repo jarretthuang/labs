bdae3d9a5d5cfe7c7c71789768ffe85d30d66309
Jarrett Huang <jarrett.huang@visier.com>
2023-02-13 21:59:29 -0800

    Implement clear button.

--- src/app/json-viewer/JsonViewer.tsx
+++ src/app/json-viewer/JsonViewer.tsx
@@ -6,16 +6,31 @@ function JsonViewer(props: any) {
   const [currentView, switchView] = useState("edit");
   const getSelectedClass = (view: ViewType) =>
     currentView === view ? "selected " : "";
-  const [text, updateText] = useState("Paste your JSON text here!");
+  const defaultText = "Paste your JSON text here!";
+  const [text, updateText] = useState(defaultText);
+  const clearDefaultText = () => {
+    if (text === defaultText) {
+      updateText("");
+    }
+  };
 
   const renderView = (viewType: string) => {
     if (viewType === "edit") {
       return (
         <div className="json-viewer-container">
-          <div className="tool-bar"></div>
+          <div className="tool-bar">
+            <div className="tool-bar-button">Copy</div>
+            <div className="tool-bar-button">Paste</div>
+            <div className="tool-bar-button">Format</div>
+            <div className="tool-bar-button">Minimize</div>
+            <div className="tool-bar-button" onClick={() => updateText("")}>
+              Clear
+            </div>
+          </div>
           <textarea
             className="main-textarea"
             value={text}
+            onClick={clearDefaultText}
             onChange={(e) => updateText(e.target.value)}
           ></textarea>
         </div>
