93a092e1679a9780c9d3018ed4615b929637bfc9
Jarrett Huang <jarrett.huang@visier.com>
2023-02-19 17:37:46 -0800

    Adjust tree item style.

--- src/app/json-viewer/JsonViewerTree.tsx
+++ src/app/json-viewer/JsonViewerTree.tsx
@@ -1,5 +1,30 @@
+import TreeView from "@mui/lab/TreeView";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TreeItem from "@mui/lab/TreeItem";
+import "./assets/css/json-viewer-tree.css";
+
 function JsonViewerTree(props) {
-  return <div>{props.jsonText}</div>;
+  return (
+    <div className="JsonViewerTree">
+      <TreeView
+        aria-label="json viewer tree"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{ flexGrow: 1, maxWidth: "max-content", overflowY: "auto" }}
+      >
+        <TreeItem nodeId="1" label="Applications">
+          <TreeItem nodeId="2" label="Calendar" />
+        </TreeItem>
+        <TreeItem nodeId="5" label="Documents">
+          <TreeItem nodeId="10" label="OSS" />
+          <TreeItem nodeId="6" label="MUI">
+            <TreeItem nodeId="8" label="index.js" />
+          </TreeItem>
+        </TreeItem>
+      </TreeView>
+    </div>
+  );
 }
 
 export default JsonViewerTree;
