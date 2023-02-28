77e308e69497edfaf49c14b8e464a2650dd86b95
Jarrett Huang <jarethuang@gmail.com>
2023-02-27 23:58:59 -0800

    Add nav bar.

--- src/app/components/nav-bar/NavBar.tsx
+++ src/app/components/nav-bar/NavBar.tsx
@@ -1,9 +1,24 @@
 import "./assets/css/NavBar.css";
+import jhLogoBW from "./assets/img/jhLogoBW.png";
+import { useState } from "react";
 
 function NavBar() {
+  const [expanded, expand] = useState(false);
+  const expandedClass = expanded ? "expanded" : "";
+
   return (
-    <nav className="NavBar">
-      <ul></ul>
+    <nav className={"NavBar " + expandedClass}>
+      <ul>
+        <li className="jh-logo" onClick={() => expand(!expanded)}>
+          <img className="jh-logo-bw" src={jhLogoBW} alt="JH" />
+        </li>
+        <li className="handle" onClick={() => expand(!expanded)}>
+          <span className="handle-symbol">+</span>
+        </li>
+        <li className="expanded-content">
+          <span>Coming soon!</span>
+        </li>
+      </ul>
     </nav>
   );
 }
