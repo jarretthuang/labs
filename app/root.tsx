54197b033a90967043bd01ec8c9187d71ce9e77b
Jarrett Huang <jarrett@jhuang.ca>
2025-09-19 16:15:40 -0700

    Add Vercel analytics

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -63,6 +64,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ScrollRestoration />
         <Scripts />
       </body>
+      <Analytics />
     </html>
   );
 }
