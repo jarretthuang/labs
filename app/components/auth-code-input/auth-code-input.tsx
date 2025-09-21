f0bd699e79bcf4d3b7dbfd854875bb58e400fa12
Jarrett Huang <jarrett@jhuang.ca>
2025-09-20 19:36:19 -0700

    Update auth-code-input.tsx

--- app/components/auth-code-input/auth-code-input.tsx
+++ app/components/auth-code-input/auth-code-input.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { AUTH_CODE_INPUT } from "~/models/components";
 
 export const meta = () => AUTH_CODE_INPUT.meta;
 export const handle = AUTH_CODE_INPUT.routeHandle;
 
 export default function AuthCodeInput() {
-  return <div>TEST</div>;
+  const [digits, setDigits] = useState(
+    new Array<number | undefined>(6).fill(undefined),
+  );
+  return (
+    <form className="flex flex-col border-1 border-gray-200 h-fit p-12 rounded-lg gap-4">
+      <div className="flex gap-2">
+        {digits.map((digit) => (
+          <input className="w-12 h-16" type="number" min={0} max={9}></input>
+        ))}
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="reset">Reset</button>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
 }
