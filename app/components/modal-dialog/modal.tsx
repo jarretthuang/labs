fb2747ab239b6f7dc5c5d0714805e3c0ae33fbe1
Jarrett Huang <jarrett@jhuang.ca>
2025-09-22 19:33:08 -0700

    Update modal.tsx

--- app/components/modal-dialog/modal.tsx
+++ app/components/modal-dialog/modal.tsx
@@ -10,7 +10,7 @@ export default function Modal({
   children: ReactNode;
 }) {
   return (
-    <div className="w-screen h-screen fixed top-0 left-0 bg-gray-400/50 flex p-2">
+    <div className="w-screen h-screen fixed top-0 left-0 bg-gray-400/50 flex p-2 z-10">
       <div className="m-auto border-1 border-gray-200 rounded-3xl w-full max-w-xl bg-white p-8 flex flex-col gap-4">
         <span className="text-2xl font-medium">{title}</span>
         <div className="flex-1">{children}</div>
