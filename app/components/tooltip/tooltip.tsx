611fc2e37ee0d40b8a51c0d6d6ddb0352153d19c
Jarrett Huang <jarrett@jhuang.ca>
2025-09-26 19:55:49 -0700

    Update tooltip.tsx

--- app/components/tooltip/tooltip.tsx
+++ app/components/tooltip/tooltip.tsx
@@ -1,4 +1,11 @@
-import { useEffect, useState, type ReactNode, type RefObject } from "react";
+import {
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  type ReactNode,
+  type RefObject,
+} from "react";
 import { createPortal } from "react-dom";
 
 export default function Tooltip({
@@ -9,6 +16,16 @@ export default function Tooltip({
   containerRef: RefObject<HTMLElement | null>;
 }) {
   const [show, setShow] = useState(false);
+
+  const tooltipPosition = useMemo(() => {
+    const current = containerRef.current;
+    return current
+      ? {
+          top: current.offsetTop + current.offsetHeight,
+          left: current.offsetLeft + current.offsetWidth,
+        }
+      : {};
+  }, [containerRef?.current]);
 
   useEffect(() => {
     const container = containerRef?.current;
@@ -28,18 +45,14 @@ export default function Tooltip({
       container.removeEventListener("touchend", onMouseLeave);
     };
   }, [containerRef]);
+
   return (
     show &&
     containerRef.current &&
     createPortal(
       <div
         className="absolute bg-gray-100 border-1 border-gray-200 px-3 py-2 rounded-md text-gray-700"
-        style={{
-          top:
-            containerRef.current.offsetTop + containerRef.current.offsetHeight,
-          left:
-            containerRef.current.offsetLeft + containerRef.current.offsetWidth,
-        }}
+        style={tooltipPosition}
       >
         {content()}
       </div>,
