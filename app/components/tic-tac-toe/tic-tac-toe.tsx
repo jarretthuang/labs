bb20ac426f641e2d15f118275eefe72b3f5d879b
Jarrett Huang <jarrett@jhuang.ca>
2025-11-05 22:07:30 -0800

    Show winning positions in tic-tac-toe

--- app/components/tic-tac-toe/tic-tac-toe.tsx
+++ app/components/tic-tac-toe/tic-tac-toe.tsx
@@ -29,6 +29,11 @@ function getInitialState(m: number, n: number): number[][] {
   return rows;
 }
 
+/**
+ * Using the slope (in degrees) of the line to represent the 4 possible crossing positions
+ */
+type CrossingDirection = 0 | -90 | -45 | -135;
+
 export function TicTacToe({ m, n, k }: TicTacToeProps) {
   const [gameState, setGameState] = useState<number[][]>(getInitialState(m, n));
 
@@ -37,6 +42,12 @@ export function TicTacToe({ m, n, k }: TicTacToeProps) {
   const MAX_ACTION_INDEX = m * n;
 
   const [winner, setWinner] = useState<number | undefined>(undefined);
+  const [winningPositions, setWinningPositions] = useState<[number, number][]>(
+    [],
+  );
+  const [winningDirection, setWinningDirection] = useState<
+    CrossingDirection | undefined
+  >(undefined);
 
   const takeAction = (rowIndex: number, colIndex: number, player: number) => {
     setGameState((prev) => {
@@ -51,43 +62,43 @@ export function TicTacToe({ m, n, k }: TicTacToeProps) {
   };
 
   const playerHasWon = (rowIndex: number, colIndex: number, player: number) => {
-    const directions: [[number, number], [number, number]][] = [
-      [
-        [0, -1],
-        [0, 1],
-      ], // ← →
-      [
-        [1, 0],
-        [-1, 0],
-      ], // ↑ ↓
-      [
-        [-1, -1],
-        [1, 1],
-      ], // ↖ ↘
-      [
-        [1, -1],
-        [-1, 1],
-      ], // ↙ ↗
+    const directions: [
+      [number, number],
+      [number, number],
+      CrossingDirection,
+    ][] = [
+      [[0, -1], [0, 1], 0], // ← →
+      [[1, 0], [-1, 0], -90], // ↑ ↓
+      [[-1, -1], [1, 1], -135], // ↖ ↘
+      [[1, -1], [-1, 1], -45], // ↙ ↗
     ];
 
     const validPosition = (r: number, c: number) => {
       return r >= 0 && r < n && c >= 0 && c < m;
     };
 
-    const countPositions = ([dr, dc]: [number, number]) => {
-      let count = 0;
+    const findPositions = ([dr, dc]: [number, number]) => {
+      let positions: [number, number][] = [];
       let r = rowIndex + dr;
       let c = colIndex + dc;
       while (validPosition(r, c) && gameState[r][c] === player) {
-        count++;
+        positions.push([r, c]);
         r += dr;
         c += dc;
       }
-      return count;
+      return positions;
     };
 
-    for (const [d1, d2] of directions) {
-      if (1 + countPositions(d1) + countPositions(d2) >= k) {
+    for (const [d1, d2, direction] of directions) {
+      const positions: [number, number][] = [
+        ...findPositions(d1),
+        [rowIndex, colIndex],
+        ...findPositions(d2),
+      ];
+      if (positions.length >= k) {
+        setWinningPositions(positions);
+        setWinningDirection(direction);
+        console.log(direction);
         return true;
       }
     }
@@ -106,7 +117,7 @@ export function TicTacToe({ m, n, k }: TicTacToeProps) {
         )}
       </div>
       <div
-        className="grid bg-gray-50 w-fit h-fit"
+        className="grid bg-gray-50 w-fit h-fit overflow-hidden"
         style={{
           gridTemplateColumns: `repeat(${m}, 1fr)`,
           gridTemplateRows: `repeat(${n}, 1fr)`,
@@ -117,7 +128,7 @@ export function TicTacToe({ m, n, k }: TicTacToeProps) {
             return (
               <button
                 key={`${rowIndex}-${colIndex}`}
-                className="w-20 h-20 flex border-1 border-gray-300 cursor-pointer hover:bg-gray-200 active:bg-gray-200 duration-200 select-none"
+                className="w-20 h-20 flex relative border-1 border-gray-300 cursor-pointer hover:bg-gray-200 active:bg-gray-200 duration-200 select-none"
                 style={{ borderRadius: 0 }} // to override the default button style
                 disabled={cellState !== undefined || winner !== undefined}
                 onClick={() => takeAction(rowIndex, colIndex, currentPlayer)}
@@ -125,6 +136,23 @@ export function TicTacToe({ m, n, k }: TicTacToeProps) {
                 <span className="m-auto text-4xl">
                   {cellState === 1 ? "⭕" : cellState === 2 ? "❌" : ""}
                 </span>
+                {winningPositions.some(
+                  ([r, c]) => r === rowIndex && c === colIndex,
+                ) && (
+                  <div className="w-full h-full absolute top-0 left-0 flex overflow-visible">
+                    <span
+                      className="flex h-2 bg-green-500/30 m-auto z-10"
+                      style={{
+                        width: "100%",
+                        transform:
+                          winningDirection === 0 || winningDirection === -90
+                            ? `rotate(${winningDirection}deg) scaleX(1.02)`
+                            : `rotate(${winningDirection}deg) scaleX(1.45)`,
+                        transformOrigin: "center center",
+                      }}
+                    ></span>
+                  </div>
+                )}
               </button>
             );
           }),
@@ -136,6 +164,8 @@ export function TicTacToe({ m, n, k }: TicTacToeProps) {
             setGameState(getInitialState(m, n));
             setActionCount(0);
             setWinner(undefined);
+            setWinningPositions([]);
+            setWinningDirection(undefined);
           }}
         >
           Reset
